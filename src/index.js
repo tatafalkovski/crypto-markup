d6df629cf5885ab198f4a2562d29e1f6dc720993
tatafalkovski <falkovski.tata@gmail.com>
2021-11-01 22:54:48 +0200

    js-dropdowns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./main.scss";
+
+var dropdowns = document.querySelectorAll('.js-dropdown');
+console.log(dropdowns);
+
+dropdowns.forEach(function(dropdown) {
+    var trigger = dropdown.querySelector('.js-trigger');
+    var target = dropdown.querySelector('.js-dropdown-target');
+
+    trigger.addEventListener('click', function(e) {
+        target.classList.toggle('visible');
+        trigger.classList.toggle('active');
+        trigger.textContent = trigger.textContent === 'Show more' ? 'Show less' : 'Show more';
+    })
+})
