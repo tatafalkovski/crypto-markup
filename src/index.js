ac49c7f1114e237fc595325b5bd7413b57066fe4
tatafalkovski <falkovski.tata@gmail.com>
2021-12-29 21:30:36 +0200

    add js button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./main.scss";
 
+// dropdown Show More
 var dropdowns = document.querySelectorAll('.js-dropdown');
 console.log(dropdowns);
 
@@ -11,5 +12,19 @@ dropdowns.forEach(function(dropdown) {
         target.classList.toggle('visible');
         trigger.classList.toggle('active');
         trigger.textContent = trigger.textContent === 'Show more' ? 'Show less' : 'Show more';
+    })
+})
+
+// dropdown deposit
+var dropdowns = document.querySelectorAll('.js-dropdown-dep');
+console.log(dropdowns);
+
+dropdowns.forEach(function(dropdown) {
+    var trigger = dropdown.querySelector('.js-trigger-dep');
+    var target = dropdown.querySelector('.js-dropdown-target-dep');
+
+    trigger.addEventListener('click', function(e) {
+        target.classList.toggle('visible');
+        trigger.classList.toggle('active');
     })
 })
